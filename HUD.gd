178cfce3b263c85dcee93076ca6d8dd9f31f0acb
STYJ <simon.tan.yu.jing@gmail.com>
2020-08-01 16:30:52 +0800

    updated settings menu

--- HUD.gd
+++ HUD.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 signal start_game
+signal new_modifier
 
 func show_message(text):
 	$VBoxContainer/Body/MessageLabel.text = text
@@ -38,12 +39,44 @@ func hide_buttons():
 	# You don't want to do $VBoxContainer/Body/Buttons.hide() because it removes
 	# the entire element from the screen. This causes the message label to be 
 	# out of alignment
-	$VBoxContainer/Body/Buttons/Start/Button.hide()
-	$VBoxContainer/Body/Buttons/Settings/Button.hide()
+	$VBoxContainer/Body/Buttons/Start/StartButton.hide()
+	$VBoxContainer/Body/Buttons/Settings/SettingsButton.hide()
 
 func show_buttons():
-	$VBoxContainer/Body/Buttons/Start/Button.show()
-	$VBoxContainer/Body/Buttons/Settings/Button.show()
+	$VBoxContainer/Body/Buttons/Start/StartButton.show()
+	$VBoxContainer/Body/Buttons/Settings/SettingsButton.show()
 
 func _on_MessageTimer_timeout():
 	$VBoxContainer/Body/MessageLabel.hide()
+
+
+func _on_SettingsButton_pressed():
+	$Settings.show()
+
+
+func _on_CloseButton_pressed():
+	$Settings.hide()
+
+
+func _on_HardcoreCheckbox_pressed():
+	var new_modifier = _calc_new_modifier(
+		$Settings/SettingsValues/Hardcore/HardcoreCheckbox.pressed,
+		int($Settings/DifficultyValue.text),
+		3
+	)
+	_set_difficulty_value(new_modifier)
+
+func _on_SpawnCheckbox_pressed():
+	var new_modifier = _calc_new_modifier(
+		$Settings/SettingsValues/Spawn/SpawnCheckbox.pressed,
+		int($Settings/DifficultyValue.text),
+		2
+	)
+	_set_difficulty_value(new_modifier)
+
+func _calc_new_modifier(property, curr, multiplier):
+	return curr * multiplier if property else curr / multiplier
+
+func _set_difficulty_value(modifier):
+	$Settings/DifficultyValue.text = " %dx" % modifier if modifier < 10 else "%dx" % modifier
+	emit_signal("new_modifier", modifier)
